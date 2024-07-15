920553779a9cea85bee5b95f6643c602844047c8
Santi <sestevez5@gmail.com>
2024-07-15 07:26:00 +0100

    .

--- src/app/_Pincel/services/api-educa.service.ts
+++ src/app/_Pincel/services/api-educa.service.ts
@@ -1,19 +1,47 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ICentro } from '../models/ICentro';
 
 const apikey='';
 const urlbase = '';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiEducaService {
 
+  apikey=''
+  tokenAutenticacion=null
+  urlBase=''
+
  
   constructor(private http: HttpClient) { }
 
-  obtenerTodosLosCursosCentros(): ICursoCentro[]|null
+  obtenerTodosLosCentros(): ICentro[]
   {
-    return null;
+    
+    let opcionesPeticion = new OpcionesPeticion();
+
+    opcionesPeticion['observe'] = 'response';
+
+    if (this.tokenAutenticacion) {
+      const headers = new HttpHeaders( {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.tokenAutenticacion}`
+      });
+      
+
   };
+
+  return [];
+  }
+}
+
+
+class OpcionesPeticion {
+  observe: string;
+  headers: HttpHeaders;
+  params: HttpParams;
 }
